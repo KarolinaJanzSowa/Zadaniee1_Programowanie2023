586db2f2921ba8334e081e068f3e7ff8633cf5b6
KarolinaJanzSowa <karolinajanz@gmail.com>
2023-06-29 21:58:13 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Zadaniee1_Programowanie2023
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Osoba osoba = new Osoba("Karolina Nowak");
+            Osoba osoba2 = new Osoba("Paulina Nowak");
+            osoba.DataUrodzenia = new DateTime(1993, 11, 1);
+            osoba.DataŚmierci = new DateTime(2000, 12, 31);
+
+
+            Console.WriteLine(osoba.Imię);
+            Console.WriteLine(osoba.Nazwisko);
+            Console.WriteLine(osoba.DataUrodzenia?.ToString("yyyy-MM-dd"));
+            Console.WriteLine(osoba.DataŚmierci?.ToString("yyyy-MM-dd"));
+            Console.WriteLine(osoba.Wiek?.TotalDays);
+            Console.WriteLine(osoba.ImięNazwisko);
+        }
+    }
+
+}
